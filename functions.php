45bda8207b9a9b6f49add90e9dbd68ef53cb2a89
wpseemol <wpseemol@gmail.com>
2024-11-09 21:52:45 +0600

    style and man js file enqueue

--- functions.php
+++ functions.php
@@ -17,7 +17,11 @@ function b2wi_scripts()
     // main stye.css file enqueue.
     wp_enqueue_style("stylesheet", get_stylesheet_uri(), [], filemtime(get_template_directory() . "/style.css"), "all");
 
-    wp_enqueue_script("main-js", get_template_directory_uri() . "/assets/main.js", [], filemtime(get_template_directory() . "/assets/main.js"), true);
+    // main-style.css file enqueue.
+    wp_enqueue_style("main-style", get_template_directory_uri() . "/assets/css/main-style.css", [], filemtime(get_template_directory() . "/assets/css/main-style.css"), "all");
+
+    // main js file enqueue.
+    wp_enqueue_script("main-js", get_template_directory_uri() . "/assets/js/main.js", [], filemtime(get_template_directory() . "/assets/js/main.js"), true);
 
 
 }
